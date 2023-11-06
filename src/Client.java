e77e23b465f3facdc862ca7e8380502a70029ff7
neoNoroNeo <mhaniifmuzaki@gmail.com>
2023-11-06 14:47:26 +0700

    Cek ombak

---
+++ src/Client.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String username;
+
+    public Client (Socket socket, String username ){
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.username = username;
+        }catch (IOException e) {
+            closeEverything(socket, bufferedWriter, bufferedReader);
+
+        }
+    }
+
+    public void sendMessage(){
+        try {
+            bufferedWriter.write(username);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            Scanner scanner = new Scanner(System.in);
+            while (socket.isConnected()) {
+                String messageToSend = scanner.nextLine();
+                bufferedWriter.write(username + ": " + messageToSend );
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        } catch (IOException e) {
+            closeEverything(socket, bufferedWriter, bufferedReader);
+        }
+    }
+    public void listenForMessage () {
+        new Thread(() -> {
+            String msgFromChat;
+
+            while (socket.isConnected()){
+                try {
+                    msgFromChat = bufferedReader.readLine();
+                    System.out.println(msgFromChat);
+                } catch (IOException e){
+                     closeEverything(socket, bufferedWriter, bufferedReader);
+                }
+            }
+        }).start();
+
+    }
+    public void closeEverything (Socket socket, BufferedWriter bufferedWriter, BufferedReader bufferedReader){
+
+        try {
+            if (bufferedReader != null){
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukan Username anda untuk memasuki chat: ");
+        String username = scanner.nextLine();
+        Socket socket = new Socket("localhost", 8080);
+        Client client = new Client(socket, username);
+        client.listenForMessage();
+        client.sendMessage();
+        System.out.print(username + ": ");
+    }
+}
---
+++ src/Framechat.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Framechat {
+    private JPanel panel1;
+    private JComboBox cbConnection;
+    private JButton btnConnect;
+    private JTextField TxtUser;
+    private JList listmessage;
+    private JTextField txtmessage;
+    private JButton btnmessage;
+}
